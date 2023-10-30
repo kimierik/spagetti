d908c189956de64d9b02777222705fea81a8c08c
kimierik <kimi.malkamaki@gmail.com>
2023-10-30 09:30:20 +0200

    added another js fizzbuzz version

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,3 +7,11 @@ const fb=(n,c=0)=>{
 }
 
 fb(100)
+
+const FizzBuzz=(n,d,c=0)=>{
+    if (c>=n){return};
+    let s="";d.forEach((item)=>{c%item[0]==0?(()=>{s+=item[1];})():(()=>{})()});
+    s?(()=>{console.log(s)})():(()=>{console.log(c)})();FizzBuzz(n,d,++c);
+}
+
+FizzBuzz(100,[[3,"fizz"],[5,"buzz"]])
